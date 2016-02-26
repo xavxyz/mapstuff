d7b2f35abd8ffabaed67bcb67b0c40e48ad4c1f5
xavizalote <xavier.cazalot@gmail.com>
2016-02-26 17:34:19 +0000

    really fancy sidebar 🎉

--- client/layout/layout.js
+++ client/layout/layout.js
@@ -4,6 +4,32 @@ Template.layout.onCreated(function() {
 	});
 });
 
+// jquery stuff goes there: Template.blabla.onRendered(...) <=> $(document).ready(...)
+Template.layout.onRendered(function () {
+	this.sidebarClosed = false;
+	this.trigger = $('.hamburger');
+	this.overlay = $('.overlay');
+});
+
+Template.layout.events({
+	'click [rel=sidebar-action]' (event, instance) {
+		if (instance.sidebarClosed == true) {
+			instance.overlay.hide();
+			instance.trigger.removeClass('is-open');
+			instance.trigger.addClass('is-closed');
+			instance.sidebarClosed = false;
+		} else {
+			instance.overlay.show();
+			instance.trigger.removeClass('is-closed');
+			instance.trigger.addClass('is-open');
+			instance.sidebarClosed = true;
+		}
+	},
+	'click [data-toggle=offcanvas]' (event, instance) {
+		$('#wrapper').toggleClass('toggled');
+	}
+});
+
 Template.layout.helpers({
 	pins () {
 		return Pins.find({});
