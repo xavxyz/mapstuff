eecb8db7dd8fe8743d0d7092de062486809d0dcd
xavizalote <xavier.cazalot@gmail.com>
2016-02-27 17:41:18 +0000

    🌴 fix before deploy to galaxy 🚀

--- client/header/header.js
+++ client/header/header.js
@@ -1,13 +1,17 @@
 import pinType from '../lib/pinType'
 
+// omg, quick & dirty
 Template.header.events({
   'click [rel=pin-image]' (event, instance) {
+    event.preventDefault();
     pinType.set('image');
   },
   'click [rel=pin-video]' (event, instance) {
+    event.preventDefault();
     pinType.set('video');
   },
   'click [rel=pin-text]' (event, instance) {
+    event.preventDefault();
     pinType.set('text');
   }
 });
