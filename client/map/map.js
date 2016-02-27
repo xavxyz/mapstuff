f0ea90ea69fd1714c89f0801eb0fc7c42332c2c0
JUDIT GRESKOVITS <juditgreskovits@JUDITs-MacBook-Air.local>
2016-02-27 12:30:05 +0000

    made a class

--- client/map/map.js
+++ client/map/map.js
@@ -1,4 +1,6 @@
-import * as MapUtils from '../modules/mapUtils.js';
+import MapUtils from '../modules/mapUtils.js';
+
+var mapUtils;
 
 function onPinMe(lng, lat) {
   console.log('onPinMe lng: ' + lng + ', lat: ' + lat);
@@ -8,20 +10,22 @@ function onMapClick(lng, lat) {
   console.log('onMapClick lng: ' + lng + ', lat: ' + lat);
 }
 
+function onMapLoaded() {
+  console.log('onMapLoaded');
+  // mapUtils.pinMe(onPinMe);
+  mapUtils.addClick(onMapClick);
+  mapUtils.addPinsForUser('JuditsUserId', [
+    {type: "text", lng: -15.42919546365738, lat: 28.149280905429094, title: "Text pin example", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris malesuada lobortis odio, vel efficitur metus aliquam nec. Etiam vehicula ipsum vel tempus fermentum."},
+    {type: "image", lng: -15.428350567817688, lat: 28.150766090719273, title:"A pretty image", text: "This pretty image shows a pretty something", link: "http://lorempixel.com/120/80/"},
+    {type: "image", lng: -15.431719422340393, lat: 28.149034949189094, title:"Another pretty image", text: "This pretty image shows another pretty something", link: "http://lorempixel.com/120/80/"},
+  ]);
+}
+
 Template.map.onRendered(function () {
 
     this.autorun(() => {
         if (Mapbox.loaded()) {
-            L.mapbox.accessToken = Meteor.settings.public.accessToken;
-            var map = L.mapbox.map('map', Meteor.settings.public.mapId);
-            MapUtils.pinMe(map, onPinMe);
-            MapUtils.addClick(map, onMapClick);
-
-            MapUtils.addPinsForUser('JuditsUserId', map, [
-              {type: "text", lng: -15.42919546365738, lat: 28.149280905429094, title: "Text pin example", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris malesuada lobortis odio, vel efficitur metus aliquam nec. Etiam vehicula ipsum vel tempus fermentum."},
-              {type: "image", lng: -15.428350567817688, lat: 28.150766090719273, title:"A pretty image", text: "This pretty image shows a pretty something", link: "http://lorempixel.com/120/80/"},
-              {type: "image", lng: -15.431719422340393, lat: 28.149034949189094, title:"Another pretty image", text: "This pretty image shows another pretty something", link: "http://lorempixel.com/120/80/"},
-            ]);
+            mapUtils = new MapUtils(Meteor.settings.public.accessToken, Meteor.settings.public.mapId, onMapLoaded);
         }
     });
 });
--- client/modules/mapUtils.js
+++ client/modules/mapUtils.js
@@ -1,127 +1,125 @@
-/*export default class MapUtils {
+export default class MapUtils {
 
-  constructor(map) {
-    this.map = map;
+  constructor(accessToken, mapId, onLoadedCallback) {
+    L.mapbox.accessToken = accessToken;
+    this.map = L.mapbox.map('map', mapId).on('ready', onLoadedCallback);
+
+    // this.pinMe(map, onPinMe);
+    // this.addClick(map, onMapClick);
   }
 
-}*/
-// add a pin
-//@pin: {type: text/image/video, lng:1, lon:1, title:"", text:"", link:""}
-export const addPin = (map, pin) => {
+  // add a pin
+  //@pin: {type: text/image/video, lng:1, lon:1, title:"", text:"", link:""}
+  addPin (pin) {
 
-  switch(pin.type) {
-    case 'text':
-      // TODO should text whether all required properties exist
-      addTextPin(map, pin.lng, pin.lat, pin.title, pin.text);
-      break;
-    case 'image':
-      // TODO should text whether all required properties exist
-      addImagePin(map, pin.lng, pin.lat, pin.title, pin.link, pin.text);
-      break;
-    case 'video':
-      // TODO should text whether all required properties exist
-      addVideoPin(map, pin.lng, pin.lat, pin.title, pin.link, pin.text);
-      break;
-    default:
-      addSimplePin(map, pin.lng, pin.lat);
+    switch(pin.type) {
+      case 'text':
+        // TODO should text whether all required properties exist
+        return this.addTextPin(pin.lng, pin.lat, pin.title, pin.text);
+      case 'image':
+        // TODO should text whether all required properties exist
+        return this.addImagePin(pin.lng, pin.lat, pin.title, pin.link, pin.text);
+      case 'video':
+        // TODO should text whether all required properties exist
+        return this.addVideoPin(pin.lng, pin.lat, pin.title, pin.link, pin.text);
+      default:
+        return this.addSimplePin(pin.lng, pin.lat);
+    }
   }
-}
 
-export const addPinsForUser = (userId, map, pins) =>{
+  addPinsForUser (userId, pins) {
 
-  pins.forEach((pin) => addPin(map, pin));
-}
+    pins.forEach((pin) => this.addPin(pin));
+  }
 
-export const addClick = (map, callback) => {
+  addClick (callback) {
 
-	map.on('click', (e) => {
+  	this.map.on('click', (e) => {
 
-    console.log('e = ', e);
-    // map.fitBounds(e.bounds);
-    addPin(map, {lng: e.latlng.lng, lat: e.latlng.lat});
-    callback(e.latlng.lng, e.latlng.lat);
-	});
-}
+      console.log('click this = ', e);
+      // map.fitBounds(e.bounds);
+      this.addPin({lng: e.latlng.lng, lat: e.latlng.lat});
+      callback(e.latlng.lng, e.latlng.lat);
+  	});
+  }
 
-export const pinMe = (map, callback) => {
+  pinMe (callback) {
 
-	map.on('locationfound', (e) => {
+  	this.map.on('locationfound', (e) => {
 
-		map.fitBounds(e.bounds);
-	  addPin(map, {lng: e.latlng.lng, lat: e.latlng.lat});
-    callback(e.latlng.lng, e.latlng.lat);
-	});
+  		this.map.fitBounds(e.bounds);
+  	  this.addPin(map, {lng: e.latlng.lng, lat: e.latlng.lat});
+      callback(e.latlng.lng, e.latlng.lat);
+  	});
 
-	if (navigator.geolocation) {
-		map.locate();
-	}
-}
+  	if (navigator.geolocation) {
+  		this.map.locate();
+  	}
+  }
 
-export const addSimplePin = (map, lng, lat) => {
+  addSimplePin (lng, lat) {
 
-  var simplePinLayer = L.mapbox.featureLayer().addTo(map);
+    var simplePinLayer = L.mapbox.featureLayer().addTo(this.map);
 
-	simplePinLayer.setGeoJSON({
-		type: 'Feature',
-		geometry: {
-			type: 'Point',
-			coordinates: [lng, lat]
-		},
-		properties: {
-			'marker-color': '#ff8888',
-			'marker-symbol': 'star'
-		}
-	});
-}
+  	simplePinLayer.setGeoJSON({
+  		type: 'Feature',
+  		geometry: {
+  			type: 'Point',
+  			coordinates: [lng, lat]
+  		},
+  		properties: {
+  			'marker-color': '#ff8888',
+  			'marker-symbol': 'star'
+  		}
+  	});
+  }
 
-// add a pin that contains text only
-export const addTextPin = (map, lng, lat, title, text) => {
-  var textPinLayer = L.mapbox.featureLayer().addTo(map);
+  // add a pin that contains text only
+  addTextPin (lng, lat, title, text) {
+    var textPinLayer = L.mapbox.featureLayer().addTo(this.map);
 
-  textPinLayer.setGeoJSON({
-    type: 'Feature',
-    geometry: {
-      type: 'Point',
-      coordinates: [lng, lat]
-    },
-    properties: {
-      'title': title,
-      'marker-color': '#ff8888',
-      'marker-symbol': 'star',
-      'text': text
-    }
-  });
+    textPinLayer.setGeoJSON({
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [lng, lat]
+      },
+      properties: {
+        'title': title,
+        'marker-color': '#ff8888',
+        'marker-symbol': 'star',
+        'text': text
+      }
+    });
 
-  textPinLayer.eachLayer(function(layer) {
+    textPinLayer.eachLayer(function(layer) {
 
-      var content = '<h2>' + layer.feature.properties.title + '<\/h2>' +
-          '<p>' + layer.feature.properties.text + '<\/p>';
-      layer.bindPopup(content);
-  });
-}
+        var content = '<h2>' + layer.feature.properties.title + '<\/h2>' +
+            '<p>' + layer.feature.properties.text + '<\/p>';
+        layer.bindPopup(content);
+    });
+  }
 
-// add a pin that contains an image and optionally text
-export const addImagePin = (map, lng, lat, title, image, text) => {
-  var imagePinLayer = L.mapbox.featureLayer().addTo(map);
+  // add a pin that contains an image and optionally text
+  addImagePin (lng, lat, title, image, text) {
+    var imagePinLayer = L.mapbox.featureLayer().addTo(this.map);
 
-  imagePinLayer.setGeoJSON({
-    type: 'Feature',
-    geometry: {
-      type: 'Point',
-      coordinates: [lng, lat]
-    },
-    properties: {
-      'title': title,
-      'marker-color': '#ff8888',
-      'marker-symbol': 'art-gallery',
-      'image': image,
-      'text': text
-    }
-  });
+    imagePinLayer.setGeoJSON({
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [lng, lat]
+      },
+      properties: {
+        'title': title,
+        'marker-color': '#ff8888',
+        'marker-symbol': 'art-gallery',
+        'image': image,
+        'text': text
+      }
+    });
 
-  imagePinLayer.eachLayer(function(layer) {
-
-    console.log('layer.feature.properties.image  = ' + layer.feature.properties.image );
+    imagePinLayer.eachLayer(function(layer) {
 
       var content = '<h2>' + layer.feature.properties.title + '<\/h2>' +
           '<img src="' + layer.feature.properties.image + '" \/>';
@@ -129,65 +127,66 @@ export const addImagePin = (map, lng, lat, title, image, text) => {
         content += '<p>' + layer.feature.properties.text + '<\/p>';
       }
       layer.bindPopup(content);
-  });
-}
+    });
+  }
 
-// add a pin that contains an video and optionally text
-export const addVideoPin = (map, lng, lat, title, video, text) => {
-  var videoPinLayer = L.mapbox.featureLayer().addTo(map);
+  // add a pin that contains an video and optionally text
+  addVideoPin (lng, lat, title, video, text) {
+    var videoPinLayer = L.mapbox.featureLayer().addTo(this.map);
 
-  videoPinLayer.setGeoJSON({
-    type: 'Feature',
-    geometry: {
-      type: 'Point',
-      coordinates: [lng, lat]
-    },
-    properties: {
-      'title': title,
-      'marker-color': '#ff8888',
-      'marker-symbol': 'theatre',
-      'video': video,
-      'text': text
-    }
-  });
-
-  videoPinLayer.eachLayer(function(layer) {
-
-      var content = '<h2>' + layer.feature.properties.title + '<\/h2>' +
-          '<img src="' + layer.feature.properties.video + '" \/>';
-      if(layer.feature.properties.text) {
-        content += '<p>' + layer.feature.properties.text + '<\/p>';
+    videoPinLayer.setGeoJSON({
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [lng, lat]
+      },
+      properties: {
+        'title': title,
+        'marker-color': '#ff8888',
+        'marker-symbol': 'theatre',
+        'video': video,
+        'text': text
       }
-      layer.bindPopup(content);
-  });
-}
+    });
 
-export const addFancyPin = (map, e) => {
+    videoPinLayer.eachLayer(function(layer) {
 
-	var featureLayer = L.mapbox.featureLayer().addTo(map);
+        var content = '<h2>' + layer.feature.properties.title + '<\/h2>' +
+            '<img src="' + layer.feature.properties.video + '" \/>';
+        if(layer.feature.properties.text) {
+          content += '<p>' + layer.feature.properties.text + '<\/p>';
+        }
+        layer.bindPopup(content);
+    });
+  }
 
-	featureLayer.setGeoJSON({
-		type: 'Feature',
-		geometry: {
-			type: 'Point',
-			coordinates: [e.latlng.lng, e.latlng.lat]
-		},
-		properties: {
-			'title': 'Here I am!',
-			'marker-color': '#ff8888',
-			'marker-symbol': 'star',
-      'from': 'Where I am now',
-      'to': 'Not yet sure',
-      'image': 'http://lorempixel.com/100/100/'
-		}
-	});
+  addFancyPin (e) {
 
-  featureLayer.eachLayer(function(layer) {
+  	var featureLayer = L.mapbox.featureLayer().addTo(this.map);
 
-      var content = '<h2>A ferry ride!<\/h2>' +
-          '<img src="' + layer.feature.properties.image + '" \/>' +
-          '<p>From: ' + layer.feature.properties.from + '<br \/>' +
-          'to: ' + layer.feature.properties.to + '<\/p>';
-      layer.bindPopup(content);
-  });
+  	featureLayer.setGeoJSON({
+  		type: 'Feature',
+  		geometry: {
+  			type: 'Point',
+  			coordinates: [e.latlng.lng, e.latlng.lat]
+  		},
+  		properties: {
+  			'title': 'Here I am!',
+  			'marker-color': '#ff8888',
+  			'marker-symbol': 'star',
+        'from': 'Where I am now',
+        'to': 'Not yet sure',
+        'image': 'http://lorempixel.com/100/100/'
+  		}
+  	});
+
+    featureLayer.eachLayer(function(layer) {
+
+        var content = '<h2>A ferry ride!<\/h2>' +
+            '<img src="' + layer.feature.properties.image + '" \/>' +
+            '<p>From: ' + layer.feature.properties.from + '<br \/>' +
+            'to: ' + layer.feature.properties.to + '<\/p>';
+        layer.bindPopup(content);
+    });
+  }
 }
