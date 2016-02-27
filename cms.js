ef2a0f8fbbaa3dc7c4a5830e4e4526857ed4d0fb
Guilliatt <dan@guilliatt.com>
2016-02-27 14:39:04 +0000

    Pins added to cms.js collection and file moved to lib

--- cms.js
+++
@@ -1,9 +0,0 @@
-AdminConfig = {
-  adminEmails: ['dan@guilliatt.com','xavier.cazalot@gmail.com','juditgreskovits@gmail.com','lukaskuhnhenn@mailbox.org']
-/*    Pins: {
-      // collection options
-    },
-    Users: {
-      // collection options
-    }*/
-};
