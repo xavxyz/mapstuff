bc7912a7e9354826bf1856fdd0d099f759c959e2
xavizalote <xavier.cazalot@gmail.com>
2016-02-29 04:53:35 +0000

    improve types list

--- client/header/header.js
+++ client/header/header.js
@@ -1,26 +1,10 @@
 import pinType from '../lib/pinType'
 
-// omg, quick & dirty
-Template.header.events({
-  'click [rel=pin-image]' (event, instance) {
-    event.preventDefault();
-    pinType.set('image');
-  },
-  'click [rel=pin-video]' (event, instance) {
-    event.preventDefault();
-    pinType.set('video');
-  },
-  'click [rel=pin-text]' (event, instance) {
-    event.preventDefault();
-    pinType.set('text');
-  }
-});
-
 Template.header.helpers({
-  selected () {
-    return pinType.get();
-  },
   pinType (type) {
     return pinType.get() === type;
+  },
+  pinTypesList () {
+    return ['text', 'image', 'video']; // XXX manage in a collection the different types of pins
   }
 });
