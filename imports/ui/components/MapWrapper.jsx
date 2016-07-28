60a892cdb93effdd3accf6787e393ae3ddc20ef3
xavcz <xavier.cazalot@gmail.com>
2016-07-28 13:02:33 +0200

    add proptypes + lint modifications

--- imports/ui/components/MapWrapper.jsx
+++ imports/ui/components/MapWrapper.jsx
@@ -1,41 +1,48 @@
-import React, { Component } from "react";
-import ReactMapboxGl, { Layer, Feature, Popup, ZoomControl } from "react-mapbox-gl";
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import ReactMapboxGl, { Layer, Feature, Popup, ZoomControl } from 'react-mapbox-gl';
 
 const { accessToken, style } = Meteor.settings.public;
 
 const containerStyle = {
-  height: "100vh",
-  width: "100%"
+  height: '100vh',
+  width: '100%',
 };
 
-export default class MapWrapper extends React.Component {
-  render() {
-    const { pins } = this.props.mapbox;
-    const worldCenter = [pins[0].lng, pins[0].lat];
-    return (
-      <ReactMapboxGl
-        style={ style }
-        center={ worldCenter }
-        zoom={ 6 }
-        accessToken={ accessToken }
-        onClick={ (map, event) => this.props.addPin(event.lngLat) }
-        containerStyle={ containerStyle }
-      >
-        <Layer
-          type="symbol"
-          id="marker"
-          layout={{ "icon-image": "marker-15" }}>
-            {
-              pins.map(pin => (
-                <Feature
-                  // basic key
-                  key={pin.lng+pin.lat}
-                  coordinates={ [pin.lng, pin.lat] }
-                />
-              ))
-            }
-        </Layer>
-      </ReactMapboxGl>
-    )
-  }
-}
+const MapWrapper = (props) => {
+  const { pins } = props.mapbox;
+  const worldCenter = [pins[0].lng, pins[0].lat];
+  return (
+    <ReactMapboxGl
+      style={ style }
+      center={ worldCenter }
+      zoom={ 6 }
+      accessToken={ accessToken }
+      onClick={ (map, event) => props.addPin(event.lngLat) }
+      containerStyle={ containerStyle }
+    >
+      <Layer
+        type="symbol"
+        id="marker"
+        layout={{ 'icon-image': 'marker-15' }}>
+          {
+            pins.map(pin => (
+              <Feature
+                // basic key
+                key={ pin.lng + pin.lat }
+                coordinates={ [pin.lng, pin.lat] }
+              />
+            ))
+          }
+      </Layer>
+    </ReactMapboxGl>
+  );
+};
+
+MapWrapper.propTypes = {
+  mapbox: React.PropTypes.object.isRequired,
+  addPin: React.PropTypes.func,
+};
+
+export default MapWrapper;
+
